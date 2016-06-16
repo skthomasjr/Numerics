2d577a8660cc72f05bbb1d689c7929dcfb4de9b3
Scott Keith Thomas, Jr <sthomas@idis.com>
2016-06-15 20:03:30 -0600

    NuGet package changes.

--- Build/NuGet/Package.cmd
+++ Build/NuGet/Package.cmd
@@ -10,10 +10,10 @@ mkdir Working\lib
 mkdir Working\tools
 
 copy Package.nuspec Working\%NUGET_PACKAGE_ID%.nuspec
-xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net35\bin\Release\* Working\lib\net35\* /s /e /y
-xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net40\bin\Release\* Working\lib\net40\* /s /e /y
-xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net45\bin\Release\* Working\lib\net45\* /s /e /y
-xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net46\bin\Release\* Working\lib\net46\* /s /e /y
-xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net461\bin\Release\* Working\lib\net461\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net35\bin\Release\%APPVEYOR_PROJECT_NAME%.* Working\lib\net35\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net40\bin\Release\%APPVEYOR_PROJECT_NAME%.* Working\lib\net40\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net45\bin\Release\%APPVEYOR_PROJECT_NAME%.* Working\lib\net45\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net46\bin\Release\%APPVEYOR_PROJECT_NAME%.* Working\lib\net46\* /s /e /y
+xcopy ..\..\Source\%APPVEYOR_PROJECT_NAME%.Net461\bin\Release\%APPVEYOR_PROJECT_NAME%.* Working\lib\net461\* /s /e /y
 
 nuget.exe pack Working\%NUGET_PACKAGE_ID%.nuspec -Version %APPVEYOR_BUILD_VERSION%
